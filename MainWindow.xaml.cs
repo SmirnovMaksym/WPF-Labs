5a5847a2f641113bfe7ea96bd5567d49f005ef6a
Maksym Smirnov <ironfire2535@gmail.com>
2024-05-14 23:44:57 +0200

    Task 4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,84 @@ namespace WPF_Labs
     /// </summary>
     public partial class MainWindow : Window
     {
+        private void CanExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (TextBox1.Text.Trim().Length > 0)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
+        private void Execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText("C:\\Users\\ironf\\OneDrive\\Робочий стіл\\University\\3 Year\\text.txt", TextBox1.Text);
+            MessageBox.Show("The file was saved!");
+        }
+
+        private void CanExecute_OpenFile(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Execute_OpenFile(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Opening file...");
+        }
+
+        private void CanExecute_DeleteText(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(TextBox1.Text);
+        }
+
+        private void Execute_DeleteText(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextBox1.Clear();
+        }
+
+        private void CanExecute_CopyText(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !string.IsNullOrEmpty(TextBox1.Text);
+        }
+
+        private void Execute_CopyText(object sender, ExecutedRoutedEventArgs e)
+        {
+            Clipboard.SetText(TextBox1.Text);
+        }
+
+        private void CanExecute_PasteText(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+
+        private void Execute_PasteText(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextBox1.Text += Clipboard.GetText();
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, Execute_Save, CanExecute_Save);
+            CommandBindings.Add(saveCommand);
+
+            CommandBinding openCommand = new CommandBinding(ApplicationCommands.Open, Execute_OpenFile, CanExecute_OpenFile);
+            CommandBindings.Add(openCommand);
+
+            CommandBinding deleteCommand = new CommandBinding(ApplicationCommands.Delete, Execute_DeleteText, CanExecute_DeleteText);
+            CommandBindings.Add(deleteCommand);
+
+            CommandBinding copyCommand = new CommandBinding(ApplicationCommands.Copy, Execute_CopyText, CanExecute_CopyText);
+            CommandBindings.Add(copyCommand);
+
+            CommandBinding pasteCommand = new CommandBinding(ApplicationCommands.Paste, Execute_PasteText, CanExecute_PasteText);
+            CommandBindings.Add(pasteCommand);
         }
+
     }
 }
